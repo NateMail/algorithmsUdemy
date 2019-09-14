e28c0e767d03e3dc3c85f8b7a0dd354a3a0f6f67
nathanmailhiot <nathanjm12@outlook.com>
2019-09-14 13:47:10 -0400

    finish naiveSearch

--- SearchingAlg/nativeStringSearch.js
+++ SearchingAlg/nativeStringSearch.js
@@ -11,3 +11,20 @@
     . If you complete the inner loop and find a match, increment the count of matches
     . Return the count 
 */
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) {
+        break;
+      }
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch('lorie loled', 'lo'));
