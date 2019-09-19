9f10993f99d972ae5c0ed5e7a1464c7b873b0bc3
nathanmailhiot <nathanjm12@outlook.com>
2019-09-19 13:40:00 -0400

    Add to fib

--- Recursion/Fib.js
+++ Recursion/Fib.js
@@ -3,3 +3,10 @@ function fib(num) {
   if (num < 2) return num;
   return fib(num - 1) + fib(num - 2);
 }
+
+// Different version
+function fibb(num) {
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+  return fibb(num - 1) + fibb(num - 2);
+}
