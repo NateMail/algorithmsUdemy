d6d7f9be8db80fad6639cb51e07f014e3d8ebfc3
nathanmailhiot <nathanjm12@outlook.com>
2019-10-08 10:22:01 -0400

    pop for doublyLinkedList

--- DataStructures/DoublyLinkedList/doublyLinkedList.js
+++ DataStructures/DoublyLinkedList/doublyLinkedList.js
@@ -26,6 +26,17 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  /*
+    Pushing pseudocode
+    +++++++++++++++++++++++
+    .Create a new node with the value passed to the function
+    .If the head property is null set the head and tail to be the newly created node
+    .If not, set the next property on the tail to be that node
+    .Set the previous property on hte newly created node to be the tail
+    .Set the tail to be the newly created node 
+    .Increment the length
+    .Return the DoublyLinkedList
+*/
   push(val) {
     let newNode = new Node(val);
     if (this.length === 0) {
@@ -39,21 +50,36 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  /*
+    Popping pseudocode
+    +++++++++++++++++++++++
+    .If there is no head, return undefined 
+    .Store the current tail in a variable 
+    .If the length is 1, set the head and tail to be null 
+    .Update the tail to be the previous Node. 
+    .Set the newTail's next to null
+    .Decrement the length 
+    .Return the value removed 
+  */
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
 
 let list = new DoublyLinkedList();
 list.push(99);
 list.push(100);
 list.push('LAST ITEM');
+list.pop();
 console.log(list);
-/*
-    Pushing pseudocode
-    +++++++++++++++++++++++
-    .Create a new node with the value passed to the function
-    .If the head property is null set the head and tail to be the newly created node
-    .If not, set the next property on the tail to be that node
-    .Set the previous property on hte newly created node to be the tail
-    .Set the tail to be the newly created node 
-    .Increment the length
-    .Return the DoublyLinkedList
-*/
