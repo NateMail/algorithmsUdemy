0b5252b3adc0d8f8e5fe8cb3bfc611db2753e240
nathanmailhiot <nathanjm12@outlook.com>
2019-10-08 10:30:21 -0400

    doublyLinkedList shift

--- DataStructures/DoublyLinkedList/doublyLinkedList.js
+++ DataStructures/DoublyLinkedList/doublyLinkedList.js
@@ -75,11 +75,41 @@ class DoublyLinkedList {
     this.length--;
     return poppedNode;
   }
+  /*
+    Shifting pseudocode 
+    +++++++++++++++++++++++
+    .If length is 0, return undefined 
+    .Store the current head property in a variable (old head)
+    .If the length is one
+        .Set the head to be null
+        .Set the tail to be null
+    .Update the head to be the next of the old head
+    .Set the head's prev property to null
+    .Set the old head's next to null
+    .Decrement the length 
+    .Return old head 
+  */
+  shift() {
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    this.head = oldHead.next;
+    this.head.prev = null;
+    oldHead.next = null;
+    this.length--;
+    return oldHead;
+  }
 }
 
 let list = new DoublyLinkedList();
-list.push(99);
+list.push('FIRST ITEM');
 list.push(100);
+list.push(101);
+list.push(102);
 list.push('LAST ITEM');
 list.pop();
+list.shift();
 console.log(list);
