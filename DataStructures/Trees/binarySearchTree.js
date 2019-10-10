02af4fdec6e3a9244faff12edc3681f4217b8c0c
nathanmailhiot <nathanjm12@outlook.com>
2019-10-10 16:19:37 -0400

    Add find method to binarySearchTree

--- DataStructures/Trees/binarySearchTree.js
+++ DataStructures/Trees/binarySearchTree.js
@@ -50,6 +50,40 @@ class BinarySearchTree {
       }
     }
   }
+  /*
+    Find pseudocode
+    +++++++++++++++++++++++
+    .Starting at the root
+        .Check if there is a root, if not - we're done searching!
+        .If ther is a root, check if the value of the new node is the value we are looking for. If we found it we are done!
+        .If not, check to see if the value is greater than or less than the value of the root
+        .If it is greater
+            .If there is, move to that node and repeat these steps
+            .If there is not, we're done searching!
+        .If it is less
+            .Check to see if there is a node to the left
+                .If there is, move to that node and repeat these steps
+                .If there is not, we're done searching!
+  */
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        // have this return true for true or false version
+        found = true;
+      }
+    }
+    // remove this line for true or false version
+    if (!found) return false;
+    // have this return false for true or false version
+    return current;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -59,4 +93,5 @@ tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
 tree.insert(8);
 tree.insert(16);
-console.log(tree.insert(17));
+tree.insert(17);
+console.log(tree);
