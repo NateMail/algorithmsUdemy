9095390992061bedac87e696dab1234dec81d11a
nathanmailhiot <nathanjm12@outlook.com>
2019-10-13 12:47:41 -0400

    hash table

---
+++ DataStructures/HashTables/hashTable.js
@@ -0,0 +1,14 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  for (let char of key) {
+    let value = char.charCodeAt(0) - 96;
+    total = (total + value) % arrayLen;
+  }
+  return total;
+}
+console.log(hash('pink', 10));
+/*
+        .Only hashes strings (we won't worry about this)
+        .Not constant time - linear in key length 
+        .Could be a little more random 
+*/
