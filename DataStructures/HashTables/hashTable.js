33b645288c5da40cb2acad75f78456fb2290faed
nathanmailhiot <nathanjm12@outlook.com>
2019-10-14 10:58:48 -0400

    set and get hash table

--- DataStructures/HashTables/hashTable.js
+++ DataStructures/HashTables/hashTable.js
@@ -1,15 +1,59 @@
-/*
-  .The prime number in the hash is helpful in spreading out the keys more uniformly.
-  .It's also helpful if the array that you're putting values into has a prime length.
-*/
-function hash(key, arrayLen) {
-  let total = 0;
-  let prime = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[1];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * prime + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  /*
+      Set Psuedocode 
+    +++++++++++++++++++++++
+  .Accepts a key and a value
+  .Hashes the key
+  .Stores teh key-value pair in the hash table array via separate chaining.
+  */
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+
+  /*
+      Get Psuedocode 
+    +++++++++++++++++++++++
+  .Accepts a key and a value
+  .Hashes the key
+  .Retrieves the key-value pair in the hash table.
+  .If the key isn't found, returns undefined 
+  */
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
-console.log(hash('pink', 13));
+
+let ht = new HashTable(17);
+ht.set('maroon', '#800000');
+ht.set('yellow', '#FFFF00');
+ht.set('olive', '#808000');
+ht.set('salmon', '#FA8072');
+ht.set('lightcoral', '#F08080');
+ht.set('mediumvioletred', '#C71585');
+ht.set('plum', '#DDA0DD');
+console.log(ht.get('salmon'));
