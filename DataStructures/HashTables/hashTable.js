67d4bf353b756771868d0f5f8b11812b6f833e1e
nathanmailhiot <nathanjm12@outlook.com>
2019-10-14 10:32:54 -0400

    hashTable refactored

--- DataStructures/HashTables/hashTable.js
+++ DataStructures/HashTables/hashTable.js
@@ -1,14 +1,15 @@
+/*
+  .The prime number in the hash is helpful in spreading out the keys more uniformly.
+  .It's also helpful if the array that you're putting values into has a prime length.
+*/
 function hash(key, arrayLen) {
   let total = 0;
-  for (let char of key) {
+  let prime = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[1];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total * prime + value) % arrayLen;
   }
   return total;
 }
 console.log(hash('pink', 10));
-/*
-        .Only hashes strings (we won't worry about this)
-        .Not constant time - linear in key length 
-        .Could be a little more random 
-*/
