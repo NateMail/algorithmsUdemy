59e180a188ff9d4716595ef80df209af173f0bb6
nathanmailhiot <nathanjm12@outlook.com>
2019-10-15 09:01:11 -0400

    add edge

--- DataStructures/Graphs/graph.js
+++ DataStructures/Graphs/graph.js
@@ -12,9 +12,26 @@ class Graph {
   addVertex(v) {
     if (!this.adjacencyList[v]) this.adjacencyList[v] = [];
   }
+
+  /*
+    Add an Edge Psuedocode
+    +++++++++++++++++++++++
+    .This function should accept two vertices, we can call them vertex1, vertex2
+    .The function should find in the adjacency list the key of vertex1 and push vertex2 to the array
+    .The function should find in the adjacency list the key of vertex2 and push vertex1 to the array
+  */
+  addEdge(v1, v2) {
+    if (this.adjacencyList[v1] && this.adjacencyList[v2])
+      this.adjacencyList[v1].push(v2);
+    if (this.adjacencyList[v2] && this.adjacencyList[v1])
+      this.adjacencyList[v2].push(v1);
+  }
 }
 
 let g = new Graph();
-g.addVertex('hello');
-
+g.addVertex('Dallas');
+g.addVertex('Tokyo');
+g.addVertex('Aspen');
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Aspen', 'Dallas');
 console.log(g);
