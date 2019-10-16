eea293a7a1b5c3100ba0e4900d7783e06ce17f4a
nathanmailhiot <nathanjm12@outlook.com>
2019-10-16 18:29:16 -0400

    DijkstrasAlg function

--- DijkstrasAlg/DikstraAgl.js
+++ DijkstrasAlg/DikstraAgl.js
@@ -1,3 +1,19 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -9,12 +25,90 @@ class WeightedGraph {
     this.adjacencyList[v1].push({ node: v2, weight });
     this.adjacencyList[v2].push({ node: v1, weight });
   }
+  /*
+    Dijkstra's Pseudocode 
+    +++++++++++++++++++++++
+.This function should accept a starting and ending vertex 
+.Create an object (we'll call it distances) and set each key to be every vertex in the adjacency list with a value of infinity, except for the starting vertex which should have a value of 0.
+.After setting a value in the distances objec, add each vertex with a priority of Infinity to the priority queue, except the starting vertex, which should have a priorityof 0 because that's where we begin.
+.Create another object called previous and set each key to be every vertex in the adjacency list with a value of null
+.Start looping as long as there is anything in the priority queue
+  .dequeue a vertex from the priority queue
+  .If that vertex is the same as the ending vertex - we are done!
+  .Otherwise loop through each alue in the adjacency list at that vertex
+    .Calculate teh distance to that vertex from the starting vertex 
+    .if the distance is less that what is currently stored in our distances object
+        .Update the distances object with new lower distance 
+        .Update the previous object to contain that vertex
+        .Enqueue the vertex with the local distance from that start node
+.Return an array with nodes in order 
+  */
+  dijkstra(start, end) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = [];
+    let smallest;
+    // build up initial state
+    for (let vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    // as long as there is something to visit
+    while (nodes.values.length) {
+      smallest = nodes.dequeue().val;
+      if (smallest === end) {
+        // WE ARE DONE
+        // BUILD PATH TO RETURN AT END
+        while (previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjacencyList[smallest]) {
+          // find the neighboring node
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          //calculate new distance to neighboring node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            // updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            // updating previous - How we got to neighbor
+            previous[nextNeighbor] = smallest;
+            // enqueue in priority queue with new priority
+            nodes.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    return path.concat(smallest).reverse();
+  }
 }
+
 let g = new WeightedGraph();
 g.addVertex('A');
 g.addVertex('B');
 g.addVertex('C');
-g.addEdge('A', 'B', 9);
-g.addEdge('A', 'C', 5);
-g.addEdge('B', 'C', 7);
-console.log(g.adjacencyList);
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B', 4);
+g.addEdge('A', 'C', 2);
+g.addEdge('B', 'E', 3);
+g.addEdge('C', 'D', 2);
+g.addEdge('C', 'F', 4);
+g.addEdge('D', 'E', 3);
+g.addEdge('D', 'F', 1);
+g.addEdge('E', 'F', 1);
+
+console.log(g.dijkstra('A', 'F'));
