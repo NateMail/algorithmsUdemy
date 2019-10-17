b1f292fd69306df83f1de604bb3d0d12cf025b83
nathanmailhiot <nathanjm12@outlook.com>
2019-10-17 10:32:34 -0400

    dynamic Fib

--- DynamicProgramming/fib.js
+++ DynamicProgramming/fib.js
@@ -4,3 +4,19 @@ function nonDyFib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 // Big O of non dynamic O(2**N)
+
+/*
+        Memoization
+.Storing the results of expensive function calls and returning the cached result when the same inputs occur again 
+*/
+
+function dynamicFib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = dynamicFib(n - 1, memo) + dynamicFib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+// Big O of dynamic O(N)
+
+console.log(dynamicFib(5));
