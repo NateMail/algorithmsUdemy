9474e5c6d39195badedb29348df652c6516ddccc
nathanmailhiot <nathanjm12@outlook.com>
2019-10-17 10:38:26 -0400

    dynamic fib tabulation

--- DynamicProgramming/fib.js
+++ DynamicProgramming/fib.js
@@ -10,13 +10,28 @@ function nonDyFib(n) {
 .Storing the results of expensive function calls and returning the cached result when the same inputs occur again 
 */
 
-function dynamicFib(n, memo = []) {
+function dynamicFibMemo(n, memo = []) {
   if (memo[n] !== undefined) return memo[n];
   if (n <= 2) return 1;
   let res = dynamicFib(n - 1, memo) + dynamicFib(n - 2, memo);
   memo[n] = res;
   return res;
 }
-// Big O of dynamic O(N)
+// Big O of dynamic memoization O(N)
 
-console.log(dynamicFib(5));
+/*
+        Tablulation 
+.Storing the result of a previous result in a "table" (usually an array)
+.Usually done using iteration 
+.Better space complexity can be achieved using tabulation 
+*/
+function dynamicFibTab(n) {
+  if (n <= 2) return 1;
+  let fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
+
+console.log(dynamicFibTab(10));
